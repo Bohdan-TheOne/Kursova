0ece9ca40adf2d09d3da00d7a7f5eec4e3fe9d66
Bohdan-TheOne <BohdanCh.the.One@gmail.com>
2020-12-14 13:53:25 +0200

    Add more Convert Methoods

--- Matrixes/Matrixes.cpp
+++ Matrixes/Matrixes.cpp
@@ -20,9 +20,38 @@ namespace Matrixes {
 			return out;
 		}
 
+
+		template<class T>
+		SparseMatrix<T> ToSparse(const CommonMatrix<T>& in) {
+			SparseMatrixNode<T>* ptr = new SparseMatrixNode<T>();
+			SparseMatrixNode<T>* start = ptr;
+			for (int i = 0; i < in.GetRowCount(); ++i) {
+				for (int j = 0; j < in.GetColCount(); ++j) {
+					if (in.GetValue(i, j)) {
+						ptr->next = new SparseMatrixNode<T>(i, j, in.GetValue(i, j));
+						ptr = ptr->next;
+					}
+				}
+			}
+			start = start->next;
+			SparseMatrix<T> out(start, in.GetRowCount(), in.GetColCount());
+			return out;
+		}
+
 		template <class T>
 		CommonMatrix<T> ToCommon(const SparseMatrix<T>& in, CommonMatrix<T>& out) {
 			out = CommonMatrix<T>(in.GetRowCount(), in.GetColCount());
+			const SparseMatrixNode<T>* ptr = in.GetFirst();
+			for (int i = 0; i < in.GetLength(); ++i) {
+				out[ptr->GetRow()][ptr->GetCol()] = ptr->GetValue();
+				ptr = ptr->next;
+			}
+			return out;
+		}
+			   
+		template <class T>
+		CommonMatrix<T> ToCommon(const SparseMatrix<T>& in) {
+			CommonMatrix<T> out(in.GetRowCount(), in.GetColCount());
 			const SparseMatrixNode<T>* ptr = in.GetFirst();
 			for (int i = 0; i < in.GetLength(); ++i) {
 				out[ptr->GetRow()][ptr->GetCol()] = ptr->GetValue();
